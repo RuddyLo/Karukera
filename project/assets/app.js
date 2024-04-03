f97b06eac31e510ff60b962c7ec78d078c1782e8
ruddylo <ratianarivoruddy@gmail.com>
2024-04-03 21:02:16 +0200

    initial staging

--- project/assets/app.js
+++ project/assets/app.js
@@ -4,7 +4,20 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-import './styles/navbar.css';
+
+import './styles/variables.css';
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/bootstrap-icons/bootstrap-icons.css';
+import './vendor/aos/aos.css';
+import './vendor/glightbox/css/glightbox.min.css';
+import './vendor/swiper/swiper-bundle.min.css';
+
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/isotope-layout/isotope.pkgd.min.js';
+import './vendor/php-email-form/validate.js';
+
+import './js/main.js';
+import './styles/main.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
