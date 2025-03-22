5c42100fffc09b576c6e8c033b79db9562d687ab
ruddylo <ratianarivoruddy@gmail.com>
2025-03-22 12:02:15 +0100

    init apartments list page

--- project/src/Controller/ApartmentController.php
+++ project/src/Controller/ApartmentController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\ApartmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/apartments')]
 class ApartmentController extends AbstractController
 {
-    #[Route('/apartments', name: 'app.apartments')]
+    public function __construct(
+        private ApartmentRepository $apartmentRepository
+    ) {
+    }
+
+
+    #[Route('/', name: 'app.apartments')]
     public function index(): Response
     {
+        $apartments = $this->apartmentRepository->findBy(['is_active' => true]);
         return $this->render('apartments/apartments.html.twig', [
-            'controller_name' => 'ApartmentController',
+            'apartments' => $apartments,
         ]);
     }
 }
