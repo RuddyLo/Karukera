c048f22a651b0c9a94d62aabff4734b9113c5975
ruddylo <ratianarivoruddy@gmail.com>
2026-04-18 11:47:27 +0300

    captcha v3

--- project/config/bundles.php
+++ project/config/bundles.php
@@ -20,4 +20,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
 ];
